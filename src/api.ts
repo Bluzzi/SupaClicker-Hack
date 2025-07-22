0da235b09899e206fb4de5c3c85f73928d4ef64b
Bluzzi <camilledugas.pro@gmail.com>
2025-07-23 00:25:11 +0200

    feat(api): add API module for handling game-related requests and security key retrieval

---
+++ src/api.ts
@@ -0,0 +1,44 @@
+import { env } from "#/env";
+import z from "zod";
+
+const URL = "https://supaclicker.vercel.app";
+const BASE_HEADERS = {
+  "content-type": "application/json",
+  "origin": "https://supaclicker.vercel.app",
+  "cookie": env.COOKIES,
+};
+
+const getCryptoKey = async () => {
+  const schema = z.object({ cryptoKey: z.string() });
+  const response = await fetch(`${URL}/api/security/key`, {
+    method: "POST",
+    headers: BASE_HEADERS,
+  });
+
+  if (!response.ok) {
+    throw Error(await response.text());
+  }
+
+  return schema.parse(await response.json());
+};
+
+const saveGame = async (payload: string) => {
+  const response = await fetch("https://supaclicker.vercel.app/api/game/save", {
+    method: "POST",
+    headers: {
+      ...BASE_HEADERS,
+      "x-game-signature": payload,
+    },
+    body: payload,
+  });
+
+  if (!response.ok) {
+    throw Error(await response.text());
+  }
+};
+
+export const api = {
+  URL: URL,
+  getCryptoKey,
+  saveGame,
+};
